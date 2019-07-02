f9df2ca7e6d1f7377f06b2ca61ec8e92036d1d52
Denis <voland2114@gmail.com>
2019-07-02 16:39:53 +0700

    first commit

---
+++ src/app/lesson-one/lesson-one.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+    selector: 'app-lesson-one',
+    styleUrls: ['./lesson-one.component.css'],
+    templateUrl:'/lesson-one.component.html',
+
+})
+export class AppLessonOne {
+    myName:string= 'myName';
+    colspan:number = 3;
+}
